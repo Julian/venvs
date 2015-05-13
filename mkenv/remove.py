6b6c4afed6677fdb7f3f6bc2c6cafff8366ebbe9
Julian Berman <Julian@GrayVines.com>
2015-05-13 13:29:53 +0200

    Error handling.

--- mkenv/remove.py
+++ mkenv/remove.py
@@ -1,5 +1,10 @@
-from mkenv._cli import CLI, Argument, Positional
+import errno
+import os
+
+from mkenv._cli import CLI, Argument, Flag, Positional
 from mkenv.common import _ROOT
+
+from bp.errors import PathError
 
 
 @CLI(
@@ -7,8 +12,18 @@ from mkenv.common import _ROOT
         kind=Positional(name="name"),
         help="remove the named virtualenv",
     ),
+    Argument(
+        kind=Flag(names=("-f", "--force")),
+        help="ignore errors if the virtualenv does not exist",
+    ),
     _ROOT,
 )
 def run(arguments, stdin, stdout, stderr):
     virtualenv = arguments["locator"].for_name(arguments["name"])
-    virtualenv.remove()
+    try:
+        virtualenv.remove()
+    except PathError as error:
+        if error.errno != errno.ENOENT:
+            raise
+        if not arguments["force"]:
+            return os.EX_NOINPUT
--- mkenv/tests/test_remove.py
+++ mkenv/tests/test_remove.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+import os
 
 from mkenv import remove
 from mkenv.tests.utils import CLIMixin
@@ -14,3 +15,13 @@ class TestMake(CLIMixin, TestCase):
         self.assertTrue(boom.exists)
         self.run_cli(["boom"])
         self.assertFalse(boom.exists)
+
+    def test_cannot_remove_non_existing_envs(self):
+        boom = self.locator.for_name("boom")
+        self.assertFalse(boom.exists)
+        self.run_cli(["boom"], exit_status=os.EX_NOINPUT)
+
+    def test_can_remove_non_existing_envs_with_force(self):
+        boom = self.locator.for_name("boom")
+        self.assertFalse(boom.exists)
+        self.run_cli(["--force", "boom"])
