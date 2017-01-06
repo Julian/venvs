676dc900fd9ed792e00fbd686c54ed00e310beb9
Julian Berman <Julian@GrayVines.com>
2017-01-06 13:16:53 -0500

    Whoops, last instance of bp.

--- mkenv/tests/test_common.py
+++ mkenv/tests/test_common.py
@@ -1,14 +1,13 @@
 from unittest import TestCase
 
-from bp.memory import MemoryFS, MemoryPath
+from filesystems import Path
 
 from mkenv.common import Locator, VirtualEnv
 
 
 class TestLocator(TestCase):
     def test_named_virtualenvs_are_children(self):
-        root = MemoryPath(fs=MemoryFS())
-        locator = Locator(root=root)
+        locator = Locator(root=Path.root())
         self.assertEqual(
             locator.for_name("one"),
             VirtualEnv(path=locator.root.descendant("one")),
