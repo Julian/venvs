ec91e113467504af7212279030e05409b76a7e4b
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-10-22 23:08:18 -0400

    call bootstrap.build() directly

--- venvs/tests/test_bootstrap.py
+++ venvs/tests/test_bootstrap.py
@@ -6,7 +6,9 @@ import sys
 
 from filesystems import native, Path
 
-from venvs.common import Locator, workon_home_env_var
+from venvs.common import workon_home_env_var
+from venvs.bootstrap import build
+import venvs.bootstrap_script
 
 
 class TestBootstrap(TestCase):
@@ -14,45 +16,15 @@ class TestBootstrap(TestCase):
         super(TestBootstrap, self).setUp()
 
         self.native_fs = native.FS()
-
         self.temporary_directory = self.native_fs.temporary_directory()
-
-        package = Path.from_string(__file__).parent().parent()
-
         self.artifact = self.temporary_directory.descendant('bootstrap.pyz')
 
-        requirements_path = package.parent().descendant('requirements.txt')
-
-        locator = Locator(root=self.temporary_directory)
-        build_venv = locator.for_name('build_venv')
-        # TODO: just stop using zipapp and create the zip manually so as
-        #       to support all regularly supported versions
-        build_venv.create()
-        build_venv.install(
-            # TODO: shouldn't this be a default or such?
-            packages=[os.environ['TOX_INI_DIR']],
-            requirements=[
-                str(requirements_path),
-            ],
-        )
-
-        path = subprocess.check_output(
-            [
-                str(build_venv.binary('python')),
-                '-c', 'import venvs; print(venvs.__file__)'
-            ]
-        ).decode('ascii') # TODO: not ascii...
-        path = Path.from_string(path).parent().parent().descendant('requirements.txt')
-        shutil.copy(str(requirements_path), str(path))
-
-        subprocess.check_call(
-            [
-                str(build_venv.binary('python')),
-                '-m', 'venvs.bootstrap',
-                '--artifact', str(self.artifact),
-                # '--script', str(package.descendant('bootstrap_script.py')),
-                '--root', os.environ['TOX_INI_DIR'],
-            ],
+        build(
+            artifact=self.artifact,
+            script=Path.from_string(
+                venvs.bootstrap_script.__file__.rstrip('c'),
+            ),
+            root=Path.from_string(os.environ['TOX_INI_DIR']),
         )
 
     def tearDown(self):
