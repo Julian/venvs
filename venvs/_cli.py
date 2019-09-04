41ffb3e3201933272faf9638e816c47b0dd8660c
Julian Berman <Julian@GrayVines.com>
2019-09-04 21:24:20 +0200

    WIP

--- venvs/_cli.py
+++ venvs/_cli.py
@@ -1,6 +1,11 @@
+import subprocess
+import sys
+
+from filesystems import Path
 import click
 
-from venvs import __version__
+from venvs import __version__, _config
+from venvs.common import _FILESYSTEM, _ROOT
 
 
 @click.group(context_settings=dict(help_option_names=["--help", "-h"]))
@@ -9,3 +14,29 @@ def main():
     """
     Centralized virtual environments.
     """
+
+
+@main.command()
+@_FILESYSTEM
+@_ROOT
+@click.option(
+    "-w", "--wheel-dir",
+    default=None,
+    help="the directory to place the built wheels",
+)
+@click.argument("env")
+def wheel(filesystem, locator, wheel_dir, env):
+    """
+    Wheel up a tracked virtual environment.
+    """
+    if wheel_dir is None:
+        wheel_dir = Path.cwd().descendant("wheelhouse")
+
+    config = _config.load(filesystem=filesystem, locator=locator)
+    section = config["virtualenv"][env]
+    subprocess.check_call(
+        [
+            sys.executable, "-m", "pip", "wheel",
+            "--wheel-dir", str(wheel_dir),
+        ] + section["install"],
+    )
