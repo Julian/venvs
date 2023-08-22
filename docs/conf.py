1463a9a7a3e87f9c98bcf23af77df99e0153acbc
Julian Berman <Julian@GrayVines.com>
2023-08-22 10:49:50 +0200

    Update the docs setup for newer Sphinx.

--- docs/conf.py
+++ docs/conf.py
@@ -1,17 +1,15 @@
-from importlib import metadata
+import importlib.metadata
 import re
-
-# -- Project information -----------------------------------------------------
 
 project = "venvs"
 author = "Julian Berman"
-copyright = "2015, " + author
+copyright = f"2015, {author}"
 
-release = metadata.version("venvs")
+release = importlib.metadata.version("venvs")
 version = release.partition("-")[0]
 
-
-# -- General configuration ---------------------------------------------------
+language = "en"
+default_role = "any"
 
 extensions = [
     "sphinx.ext.autodoc",
@@ -23,39 +21,10 @@ extensions = [
     "sphinxcontrib.spelling",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+pygments_style = "lovelace"
+pygments_dark_style = "one-dark"
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-# The reST default role (used for this markup: `text`) to use for all documents
-default_role = "any"
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "furo"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
-
-
-# -- Extension configuration -------------------------------------------------
-
-# -- Options for intersphinx extension ---------------------------------------
-
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"https://docs.python.org/3/": None}
-
-# -- Options for the linkcheck builder ------------------------------------
 
 
 def entire_domain(host):
@@ -63,12 +32,26 @@ def entire_domain(host):
 
 
 linkcheck_ignore = [
+    entire_domain("img.shields.io"),
+    "https://github.com/.*#.*",
     "https://github.com/Julian/venvs/actions",
     "https://github.com/Julian/venvs/workflows/CI/badge.svg",
 ]
 
-linkcheck_anchors = False
+# = Extensions =
 
-# -- Options for sphinxcontrib-spelling -----------------------------------
+# -- autodoc --
+
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+}
+# -- intersphinx --
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/", None),
+}
+
+# -- sphinxcontrib-spelling --
 
 spelling_word_list_filename = "spelling-wordlist.txt"
